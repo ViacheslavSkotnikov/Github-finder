e5d84a48bb2341231f33dc35ba20185a9dc8db8d
Slava <you@example.com>
2020-01-04 19:15:42 +0300

    first commit

---
+++ github.js
@@ -0,0 +1,25 @@
+class Github {
+  constructor() {
+    this.client_id = "8ab9b8fd9e01365beace";
+    this.client_secret = "193f0bca6f00fd6129b0f729fef1163322518662";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    };
+  }
+}
